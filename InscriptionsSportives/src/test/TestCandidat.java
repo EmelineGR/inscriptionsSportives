fa197c88cbdac2b7d4c007e42b13afbfaf50f0b0
bkasteller <bryan.kasteller@gmail.com>
2018-11-19 16:32:21 +0100

    supp d'un test candidat

--- InscriptionsSportives/src/test/TestCandidat.java
+++ InscriptionsSportives/src/test/TestCandidat.java
@@ -2,13 +2,41 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
+import inscriptions.Competition;
+import inscriptions.Equipe;
+import inscriptions.Inscriptions;
+import inscriptions.Personne;
+
 class TestCandidat {
-
+	Inscriptions inscriptions = Inscriptions.getInscriptions();
+	Competition testcompetition = inscriptions.createCompetition("Competition", null, false);
+	Personne test = inscriptions.createPersonne("nom", "prenom", "mail");
+	Equipe testEquipe = inscriptions.createEquipe("Les Manouches");
+	Competition testcompetition2 = inscriptions.createCompetition("Competition", null, false);
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void getCompetitions() {
+		//assertTrue(inscriptions.getPersonnes().contains(test));
 	}
-
+	
+	@Test
+	public void getNom() {
+		//assertTrue(inscriptions.getPersonnes().contains(test));
+	}
+	
+	@Test
+	public void setNom() {
+		//assertTrue(inscriptions.getPersonnes().contains(test));
+	}
+	
+	@Test
+	public void delete() {
+//		testcompetition.createCompetition("test", localDate, 1);
+		testcompetition.delete();
+		assertTrue(inscriptions.getCompetitions().isEmpty());
+	}
 }

3d7bbf3ff4de184b2f055be141c8472178b1cac0
regisGRB <regis.grumberg@gmail.com>
2018-11-19 14:32:47 +0100

    test3.0

---
+++ InscriptionsSportives/src/test/TestCompetition.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import inscriptions.Competition;
+import inscriptions.Equipe;
+import inscriptions.Inscriptions;
+import inscriptions.Personne;
+
+class TestCompetition {
+	Inscriptions inscriptions = Inscriptions.getInscriptions();
+	Competition testcompetition = inscriptions.createCompetition("Competition", null, false);
+	Personne test = inscriptions.createPersonne("nom", "prenom", "mail");
+	Equipe testEquipe = inscriptions.createEquipe("Les Manouches");
+	Competition testcompetition2 = inscriptions.createCompetition("Competition", null, false);
+	
+	@Test
+	public void inscrioOuverte() {
+		LocalDate eme = LocalDate.of(2014,3, 1);
+testcompetition.setDateCloture(eme);
+assertFalse(testcompetition.inscriptionsOuvertes());
+	}
+}
